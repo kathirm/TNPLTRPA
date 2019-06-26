e7d12816e3132c8c9926fe55aac23b68d5938bbb
kathirm <47733031+kathirm@users.noreply.github.com>
2019-06-26 18:55:27 +0530

    Update py_mongo_auth.py

--- py_mongo_auth.py
+++ py_mongo_auth.py
@@ -4,8 +4,8 @@ import pymongo
 
 def mongo_auth():
     try:
-        conn = pymongo.MongoClient('mongodb://userAdmin:terafastnetworks@10.6.7.28:27017')
-#       client = MongoClient('mongodb://userAdmin:terafastnetworks@10.6.7.28:27017'); 
+        conn = pymongo.MongoClient('mongodb://xxxxxxxxxxxxx:xxxxxxxx@1111111.11111.111111:111.111.4111')
+#       client = MongoClient('mongodb://:'); 
         db = conn["Superstar"]
         colName = db["ObjectRepos"]
         query = {"objectType" : "Json"}
