58b2f2fe842f45542ad470c9becc9587039af5ff
Kathir <mkathir@terafastnet.com>
2019-12-18 19:42:36 +0530

    urls

---
+++ basi_url.py
@@ -0,0 +1,353 @@
+@git 
+
+    1. https://github.com/ravilak/trpa
+
+@Python Get Today’s Current Date and Time
+
+    1. https://www.cyberciti.biz/faq/howto-get-current-date-time-in-python/
+
+@Multiple Function Arguments
+
+    1. https://www.learnpython.org/en/Multiple_Function_Arguments
+
+@Python IF, ELSE, ELIF, Nested IF & Switch Case Statement
+
+    1. https://www.guru99.com/if-loop-python-conditional-structures.html
+
+@Send data from django views to angularjs controller
+
+    1. https://stackoverflow.com/questions/36713518/send-data-from-django-views-to-angularjs-controller
+
+@setting up a user login in python / django using json and have no idea where to start
+
+    1. https://stackoverflow.com/questions/11891322/setting-up-a-user-login-in-python-django-using-json-and-have-no-idea-where-to
+
+@User Authentication with Django REST Framework
+
+    1. https://michaelwashburnjr.com/2017/11/14/django-user-authentication/
+
+@How do I create a login API using Django Rest Framework?
+
+    1. https://stackoverflow.com/questions/21317899/how-do-i-create-a-login-api-using-django-rest-framework
+
+@Django Tutorial Part 8: User authentication and permissions
+
+    1. https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Authentication
+
+@How to properly pass JSON in django
+
+    1. https://stackoverflow.com/questions/45117963/how-to-properly-pass-json-in-django
+
+@Django REST Framework: Authentication and Permissions
+
+    1. http://polyglot.ninja/django-rest-framework-authentication-permissions/http://polyglot.ninja/django-rest-framework-authentication-permissions/
+
+@Build a REST API with Django – A Test Driven Approach: Part 1
+
+    1. https://scotch.io/tutorials/build-a-rest-api-with-django-a-test-driven-approach-part-1
+
+@Create a Django API in Under 20 Minutes
+
+    1. https://codeburst.io/create-a-django-api-in-under-20-minutes-2a082a60f6f3
+
+@Class-based Views
+
+    1. https://www.django-rest-framework.org/api-guide/views/
+
+@Create A REST API With JSON Server
+
+    1. https://codingthesmartway.com/create-a-rest-api-with-json-server/
+
+@How to Use RESTful APIs with Django
+
+    1. https://simpleisbetterthancomplex.com/tutorial/2018/02/03/how-to-use-restful-apis-with-django.html
+
+@Python django.http.JsonResponse() Examples
+
+    1. https://www.programcreek.com/python/example/59542/django.http.JsonResponse
+
+@Using the Django authentication system
+
+    1. https://docs.djangoproject.com/en/2.0/topics/auth/default/
+
+@How to handle user authentication in Python Django
+
+    1. https://www.freecodecamp.org/news/user-authentication-in-django-bae3a387f77d/
+
+@Python django.http.response.HttpResponse() Examples
+
+    1. https://www.programcreek.com/python/example/97055/django.http.response.HttpResponse
+
+
+@Getting started with VMwares ESXi/vSphere API in Python
+
+    1. https://www.jacobtomlinson.co.uk/posts/2016/getting-started-with-vmwares-esxi/vsphere-api-in-python/
+
+@Pyvmomi get folders name
+
+    1. https://stackoverflow.com/questions/39706470/pyvmomi-get-folders-name
+
+@How to find vm in particular folder using pyvmomi
+
+    1. https://stackoverflow.com/questions/37950968/how-to-find-vm-in-particular-folder-using-pyvmomi?rq=1
+
+@JOSN FORMATTER
+
+    1. https://jsonformatter.org/
+
+@Convert Case Text Generator Tool
+
+    1. https://convertcase.net/
+
+@Read checkbox in Flask
+
+    1. https://coderwall.com/p/pkthoa/read-checkbox-in-flask
+    2. http://jsfiddle.net/LCU4C/2/
+    3. http://jsfiddle.net/Qj87u/4/
+    4. http://jsfiddle.net/59hHK/183/
+
+@HTML Frames Are Obsolete In HTML5: Here’s How To Make Them Responsive
+
+    1. https://html.com/frames/https://html.com/frames/
+
+@FLASK Setting up the Project Structure
+
+    1. https://code.tutsplus.com/tutorials/an-introduction-to-pythons-flask-framework--net-28822
+
+@Building an Angular 5 Application Step-By-Step
+
+    1. https://dzone.com/articles/building-angular5-application-step-by-step
+
+@A modern responsive front-end framework based on Material Design
+
+    1. https://materializecss.com/
+
+@MySQL Python tutorial
+
+    1. http://zetcode.com/db/mysqlpython/
+
+@Python read, validate and import CSV/JSON file to MySQL
+
+    1. https://blog.softhints.com/python-read-validate-and-import-csv-json-file-to-mysql/
+
+@MongoDB Update() Document with Example
+
+    1. https://www.guru99.com/mongodb-update-document.html
+
+@img2pdf 0.3.3
+
+    1. https://pypi.org/project/img2pdf/
+
+@ashaywalke/resume-parser
+
+    1. https://github.com/ashaywalke/resume-parser
+
+@keithweaver/python-aws-s3
+
+    1. https://github.com/keithweaver/python-aws-s3
+
+@awsdocs/aws-doc-sdk-examples
+
+    1. https://github.com/awsdocs/aws-doc-sdk-examples/blob/master/python/example_code/s3/s3-python-example-delete-bucket-policy.pyhttps://github.com/awsdocs/aws-doc-sdk-examples/blob/master/python/example_code/s3/s3-python-example-delete-bucket-policy.py
+
+@Downloading a file from an s3 Bucket to the USERS computer
+
+    1. https://stackoverflow.com/questions/43215889/downloading-a-file-from-an-s3-bucket-to-the-users-computer
+
+@Retrieve S3 file as Object instead of downloading to absolute system path
+
+    1. https://stackoverflow.com/questions/29378763/how-to-save-s3-object-to-a-file-using-boto3
+
+@Extract text with OCR for all image types in python using pytesseract
+
+    1. https://medium.com/@MicroPyramid/extract-text-with-ocr-for-all-image-types-in-python-using-pytesseract-ec3c53e5fc3a
+
+@text extraction from image using opencv python
+
+    1. https://www.google.com/search?rlz=1C1CHBF_enIN783IN783&q=text+extraction+from+image+using+opencv+python&sa=X&ved=2ahUKEwjgx6__3qbfAhWciXAKHbYAD7YQ1QIoAHoECAUQAQ&biw=1366&bih=657https://www.google.com/search?rlz=1C1CHBF_enIN783IN783&q=text+extraction+from+image+using+opencv+python&sa=X&ved=2ahUKEwjgx6__3qbfAhWciXAKHbYAD7YQ1QIoAHoECAUQAQ&biw=1366&bih=657
+
+@Docker compose script complaining about a python module import
+
+    1. https://stackoverflow.com/questions/53257338/docker-compose-script-complaining-about-a-python-module-import
+
+@cloud-init is failing due to urllib3 dependency #3678
+
+    1. https://github.com/aws/aws-cli/issues/3678
+
+@How to Setup Selenium with ChromeDriver on Ubuntu 18.04 & 16.04
+
+    1. https://tecadmin.net/setup-selenium-chromedriver-on-ubuntu/
+
+@handwriting-recognition
+
+    1. https://github.com/topics/handwriting-recognition?o=asc&s=updated
+
+
+@HANDWRITTEN
+
+    1. https://www.linkedin.com/feed/update/urn:li:activity:6454087778501259264
+
+    2. VaibhavKhamgaonkar/OCR
+
+    3. https://github.com/VaibhavKhamgaonkar/OCR
+
+    4. https://www.kaggle.com/vaibhao/handwritten-characters
+
+    5. https://cbrownley.wordpress.com/2016/06/26/parsing-pdfs-in-python-with-tika/
+
+    6. https://www.digitalocean.com/community/tutorials/how-to-remove-docker-images-containers-and-volumes
+
+    7. https://github.com/vermavinay982/OCR_pdf_ENGLISH
+
+    8. https://www.r2docuo.com/en/video-tutorials/tutorial-16
+
+    9. https://support.filevine.com/hc/en-us/articles/360004150132-Doc-Generation-Microsoft-Word-Step-by-Step-Guide-
+
+    10. http://pingax.com/trick-convert-mongo-shell-query-equivalent-java-objects/
+
+
+@MongoDDB Aggregate
+
+    1. https://stackoverflow.com/questions/40307659/get-average-from-mongo-collection-using-aggrerate
+
+@age calculator in python from date (mm/dd/yyyy) and print age in years only
+
+    1. https://stackoverflow.com/questions/22344244/age-calculator-in-python-from-date-mm-dd-yyyy-and-print-age-in-years-only
+
+@PyMongo/Mongoengine equivalent of mongodump
+
+    1. https://stackoverflow.com/questions/24610484/pymongo-mongoengine-equivalent-of-mongodump
+
+@How to insert HTML into Mongodb?
+
+    1. https://groups.google.com/forum/#!topic/mongodb-user/vw00sM1v5o4
+
+@Getting the difference (in values) between two dictionaries in python
+
+    1. https://stackoverflow.com/questions/43371951/getting-the-difference-in-values-between-two-dictionaries-in-python
+
+@python dictionary match key values in two dictionaries
+
+    1. https://stackoverflow.com/questions/10832373/python-dictionary-match-key-values-in-two-dictionaries
+
+@substract values two dictionaries python
+    
+    1. https://stackoverflow.com/questions/9960133/substract-values-two-dictionaries-python
+
+@python create html table from dict
+
+    1. https://stackoverflow.com/questions/39412679/python-create-html-table-from-dict
+
+@python : HTML table creation from dictionary items
+    
+    1. https://stackoverflow.com/questions/49952480/python-html-table-creation-from-dictionary-items
+
+@MQTT Python With Paho-MQTT (Beginner’s Guide With Example)
+
+    1. https://mntolia.com/mqtt-python-with-paho-mqtt-client/
+
+@Material Design
+
+    1. http://demo.geekslabs.com/materialize/v3.1/css-icons.html
+
+    2. http://demo.geekslabs.com/materialize/v3.1/app-email.html#!
+
+@Running Multiple Python Files from One Command Line
+
+    1. https://www.raspberrypi.org/forums/viewtopic.php?t=97851
+
+@border-radius
+
+    1. https://developer.mozilla.org/en-US/docs/Web/CSS/border-radius
+
+@ImportError: No module named requests
+
+    1. https://stackoverflow.com/questions/17309288/importerror-no-module-named-requests
+
+@Get month name from number
+
+    1. https://stackoverflow.com/questions/6557553/get-month-name-from-number
+
+@How to calculate number of days between two given dates?
+
+    1. https://stackoverflow.com/questions/151199/how-to-calculate-number-of-days-between-two-given-dates
+
+@How do I get a string format of the current date time, in python?
+
+    1. https://stackoverflow.com/questions/3316882/how-do-i-get-a-string-format-of-the-current-date-time-in-python
+
+@Convert python datetime to timestamp in milliseconds
+
+    1. https://stackoverflow.com/questions/41635547/convert-python-datetime-to-timestamp-in-milliseconds
+
+@Legacy tokens
+
+    1. https://api.slack.com/custom-integrations/legacy-tokens
+
+@camunda-consulting/code
+
+    1. https://github.com/camunda-consulting/code/tree/master/snippets/camunda-tasklist-examples/camunda-angular-app/src/app
+
+@camunda/camunda-bpm-examples
+
+    1. https://github.com/camunda/camunda-bpm-examples/tree/master/usertask/task-form-embedded/src/main/webapp
+
+@Face Recognition using Artificial Intelligence
+
+    1. https://towardsdatascience.com/face-recognition-using-artificial-intelligence-fffa3b20ad5f
+
+@neha01/FaceRecognition
+    
+    1. https://github.com/neha01/FaceRecognition
+    
+    2. https://github.com/neha01/FaceRecognition/blob/20692e3898b2f1018f041e7061f20333bfad6b2c/faceRecognition.py#L46
+
+@Real-Time Face Recognition: An End-To-End Project
+    
+    1. https://towardsdatascience.com/real-time-face-recognition-an-end-to-end-project-b738bb0f7348
+
+    2. https://github.com/Mjrovai/OpenCV-Face-Recognition
+
+    3. https://www.hackster.io/mjrobot/real-time-face-recognition-an-end-to-end-project-a10826
+
+    4. https://github.com/Mjrovai/OpenCV-Face-Recognition/tree/master/FacialRecognition
+
+    5. https://github.com/hyperledger/fabric-sdk-py
+
+@How To Install and Use PostgreSQL on Ubuntu 18.04
+
+    1. https://www.digitalocean.com/community/tutorials/how-to-install-and-use-postgresql-on-ubuntu-18-04
+
+    2. https://odoo-saas-tools.readthedocs.io/en/10.0/api.html
+
+@odoo Create Users Via API?Create Users Via API?
+
+    1. https://www.odoo.com/forum/help-1/question/create-users-via-api-85406
+
+@How to create a new user and give him access via API
+
+    1. https://www.odoo.com/forum/help-1/question/how-to-create-a-new-user-and-give-him-access-via-api-96337
+
+@Create and manage Odoo databases with ERPpeek or XML-RPC
+
+    1. https://www.odoo.yenthevg.com/creating-and-managing-databases-with-erppeek-or-xml-rpc/
+
+@ODOO PAYMENT GATEWAY
+
+    1. https://www.cybrosys.com/blog/payment-gateway-integration-odoo
+
+@Linux: I want to see a list of Postgres databases via the terminal
+
+    1. https://dba.stackexchange.com/questions/191767/linux-i-want-to-see-a-list-of-postgres-databases-via-the-terminal
+
+@Home / PostgreSQL Administration / PostgreSQL Show Databases
+PostgreSQL Show Databases
+
+    1. http://www.postgresqltutorial.com/postgresql-show-databases/
+
+@How can I find the admin login and password after installing Odoo?
+
+    1. https://www.odoo.com/es_ES/forum/ayuda-1/question/how-can-i-find-the-admin-login-and-password-after-installing-odoo-116357
+
+
+    
