4e2c339eca4f607ed3090714887bc867a757cbe7
Kathir <mkathir@terafastnet.com>
2019-12-23 12:03:56 +0530

    git configuration

---
+++ git_config/git_config.py
@@ -0,0 +1,29 @@
+
+    @GIT_REPOS_CONFIGURATION_IN_LOCAL_ENVIRONMENT
+
+@step_1 :
+
+    1. Go into the directory containing the project.
+
+    2. Type git init.
+
+    3. git remote add origin https://github.com/username/new_repo (example)
+
+    4. INput Your username / password 
+
+    5. git pull origin master
+
+    6. Create one sample file  & add example code
+
+    7. git add (files)
+
+    8. git commit -m"(put some msg)"
+
+    9. git push origin master
+
+
+
+@Complete
+
+
+    @Ref :  https://kbroman.org/github_tutorial/pages/init.html
